099b91e4affeeddb6dd120e31525be4eccdd7fd4
Cdm2883 <Cdm2883@hotmail.com>
2024-09-24 00:53:03 +0800

    init: empty gradle project

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+rootProject.name = "MarkBook"
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+    }
+}
